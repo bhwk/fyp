d55c5515139d87b57bdcf90df410a90ea2ebba38
bhwk <120708160+bhwk@users.noreply.github.com>
2024-10-09 16:30:12 +0800

    try to form dataset

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-from pprint import pprint
+from typing import Any
 from fhir.resources.R4B.bundle import Bundle
 from fhir.resources.R4B.patient import Patient
 import pathlib
+import ray
 
 
-def parse_bundle_entries(bundle: Bundle, patients: list):
+def parse_bundle_entries(path: dict[str, Any]):
+    file = path["path"]
+    bundle = Bundle.parse_file(file)
+
     for entry in bundle.entry:
         if isinstance(entry.resource, Patient):
             patient = entry.resource
@@ -16,19 +20,25 @@ def parse_bundle_entries(bundle: Bundle, patients: list):
                 "birthDate": patient.birthDate.isoformat()
                 if patient.birthDate
                 else None,
-                "address": patient.address[0].text if patient.address else None,
+                "address": {
+                    "line": patient.address[0].line[0],
+                    "city": patient.address[0].city,
+                    "state": patient.address[0].state,
+                    "postalCode": patient.address[0].postalCode,
+                    "country": patient.address[0].country,
+                }
+                if patient.address
+                else None,
                 "phone": patient.telecom[0].value if patient.telecom else None,
             }
-            patients.append(patient_data)
+            return patient_data
 
 
-patients = []
+def create_ds():
+    # create our bundle ds
+    BUNDLE_DIR = pathlib.Path("./fhir/")
+    ds = ray.data.from_items([{"path": path} for path in BUNDLE_DIR.rglob("*")])
 
-
-files = pathlib.Path("fhir").glob("*")
-for file in files:
-    print(f"Parsing {file}...")
-    bundle = Bundle.parse_file(file)
-    parse_bundle_entries(bundle, patients)
-
-pprint(patients)
+    # build patient dataset
+    patient_ds = ds.map(parse_bundle_entries).take_all()
+    print(patient_ds)
