f32f6fc2824f886659db87c18db0850bfe7d2a06
bhwk <120708160+bhwk@users.noreply.github.com>
2024-10-10 10:51:42 +0800

    try flattening fhir instead

--- test.py
+++ test.py
@@ -2,7 +2,78 @@ from typing import Any
 from fhir.resources.R4B.bundle import Bundle
 from fhir.resources.R4B.patient import Patient
 import pathlib
+import glob
 import ray
+import os
+import json
+
+FLAT_FILE_PATH = "./temp/flat"
+BUNDLE_DIR = pathlib.Path("./fhir/")
+
+
+def flatten_fhir(json):
+    out = {}
+
+    def flatten(json_to_flat, name=""):
+        # If the Nested key-value
+        # pair is of dict type
+        if type(json_to_flat) is dict:
+            for sub_attrib in json_to_flat:
+                flatten(json_to_flat[sub_attrib], name + sub_attrib + "_")
+
+        # If the Nested key-value
+        # pair is of list type
+        elif type(json_to_flat) is list:
+            for i, sub_json in enumerate(json_to_flat):
+                flatten(sub_json, name + str(i) + "_")
+        else:
+            out[name[:-1]] = json_to_flat
+
+    flatten(json)
+    return out
+
+
+def filter_patient(entry):
+    return entry["resource"]["resourceType"] == "Patient"
+
+
+def find_patient(bundle):
+    patients = list(filter(filter_patient, bundle["entry"]))
+    if len(patients) < 1:
+        raise Exception("No patient found")
+    else:
+        patient = patients[0]["resource"]
+        patient_id = patient["id"]
+        patient_given_name = patient["name"][0]["given"][0]
+        patient_family_name = patient["name"][0]["family"]
+        return {
+            "PatientFirstName": patient_given_name,
+            "PatientFamilyName": patient_family_name,
+            "PatientID": patient_id,
+        }
+
+
+def flat_to_string(flattened_json):
+    output = ""
+    for entry in flattened_json:
+        output += f"{entry} is {flattened_json[entry]}."
+    return output
+
+
+def flatten_bundle(bundle_path):
+    file_name = bundle_path.stem
+    with open(bundle_path) as raw:
+        bundle = json.load(raw)
+
+        patient = find_patient(bundle)
+        flat_patient = flatten_fhir(patient)
+
+        for i, entry in enumerate(bundle["entry"]):
+            flat_entry = flatten_fhir(entry["resource"])
+            with open(f"{FLAT_FILE_PATH}/{file_name}_{i}.txt", "w") as out:
+                out.write(
+                    f"{flat_to_string(flat_patient)}\n{flat_to_string(flat_entry)}"
+                )
 
 
 def parse_bundle_entries(path: dict[str, Any]):
@@ -36,7 +107,6 @@ def parse_bundle_entries(path: dict[str, Any]):
 
 def create_ds():
     # create our bundle ds
-    BUNDLE_DIR = pathlib.Path("./fhir/")
     ds = ray.data.from_items([{"path": path} for path in BUNDLE_DIR.rglob("*")])
 
     # build patient dataset
@@ -44,4 +114,7 @@ def create_ds():
     patient_ds.write_parquet("./patient_ds/")
 
 
-create_ds()
+if not os.path.exists(FLAT_FILE_PATH):
+    os.mkdir(FLAT_FILE_PATH)
+for file in BUNDLE_DIR.glob("*"):
+    flatten_bundle(file)
