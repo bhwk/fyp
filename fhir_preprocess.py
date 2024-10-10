24bbbb347b8c00d64fe24ce1a2efdbeb857f625f
bhwk <120708160+bhwk@users.noreply.github.com>
2024-10-11 01:05:50 +0800

    change name, fix flattening format

--- fhir_preprocess.py
+++ fhir_preprocess.py
@@ -1,11 +1,11 @@
 from typing import Any
-from fhir.resources.R4B.bundle import Bundle
-from fhir.resources.R4B.patient import Patient
 import pathlib
 import glob
 import ray
+import re
 import os
 import json
+
 
 FLAT_FILE_PATH = "./temp/flat"
 BUNDLE_DIR = pathlib.Path("./fhir/")
@@ -19,13 +19,15 @@ def flatten_fhir(json):
         # pair is of dict type
         if type(json_to_flat) is dict:
             for sub_attrib in json_to_flat:
-                flatten(json_to_flat[sub_attrib], name + sub_attrib + "_")
+                flatten(json_to_flat[sub_attrib], name + sub_attrib + " ")
 
         # If the Nested key-value
         # pair is of list type
         elif type(json_to_flat) is list:
-            for i, sub_json in enumerate(json_to_flat):
-                flatten(sub_json, name + str(i) + "_")
+            i = 0
+            for sub_json in json_to_flat:
+                flatten(sub_json, name + str(i) + " ")
+                i += 1
         else:
             out[name[:-1]] = json_to_flat
 
@@ -56,7 +58,7 @@ def find_patient(bundle):
 def flat_to_string(flattened_json):
     output = ""
     for entry in flattened_json:
-        output += f"{entry} is {flattened_json[entry]}."
+        output += f"{entry} {flattened_json[entry]}. "
     return output
 
 
@@ -74,44 +76,6 @@ def flatten_bundle(bundle_path):
                 out.write(
                     f"{flat_to_string(flat_patient)}\n{flat_to_string(flat_entry)}"
                 )
-
-
-def parse_bundle_entries(path: dict[str, Any]):
-    file = path["path"]
-    bundle = Bundle.parse_file(file)
-
-    for entry in bundle.entry:
-        if isinstance(entry.resource, Patient):
-            patient = entry.resource
-            patient_data = {
-                "id": patient.id,
-                "name": patient.name[0].given[0] if patient.name else None,
-                "family": patient.name[0].family if patient.name else None,
-                "gender": patient.gender,
-                "birthDate": patient.birthDate.isoformat()
-                if patient.birthDate
-                else None,
-                "address": {
-                    "line": patient.address[0].line[0],
-                    "city": patient.address[0].city,
-                    "state": patient.address[0].state,
-                    "postalCode": patient.address[0].postalCode,
-                    "country": patient.address[0].country,
-                }
-                if patient.address
-                else None,
-                "phone": patient.telecom[0].value if patient.telecom else None,
-            }
-            return patient_data
-
-
-def create_ds():
-    # create our bundle ds
-    ds = ray.data.from_items([{"path": path} for path in BUNDLE_DIR.rglob("*")])
-
-    # build patient dataset
-    patient_ds = ds.map(parse_bundle_entries)
-    patient_ds.write_parquet("./patient_ds/")
 
 
 if not os.path.exists(FLAT_FILE_PATH):
