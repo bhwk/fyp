5e59872a4ef9c2b3a220622bbd1fd8ebcf7b8339
bhwk <120708160+bhwk@users.noreply.github.com>
2024-10-16 18:02:48 +0800

    fix some stuff woops

--- query_db.py
+++ query_db.py
@@ -1,7 +1,11 @@
+import llama_index.core
+from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core import (
     VectorStoreIndex,
     Settings,
+    get_response_synthesizer,
 )
+from llama_index.core.indices.vector_store import VectorIndexRetriever
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
@@ -12,6 +16,8 @@ from llama_index.core.callbacks import (
     CallbackManager,
     LlamaDebugHandler,
 )
+
+llama_index.core.set_global_handler("simple")
 
 
 def get_db(callback_manager):
@@ -25,7 +31,6 @@ def get_db(callback_manager):
     embed_model = HuggingFaceEmbedding(
         model_name="BAAI/bge-small-en-v1.5",
         callback_manager=callback_manager,
-        parallel_process=True,
     )
     index = VectorStoreIndex.from_vector_store(vector_store, embed_model=embed_model)
 
