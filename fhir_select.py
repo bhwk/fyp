6e7dfe44cf97f67c2f34cb17bbd56db2f4766ccc
bhwk <120708160+bhwk@users.noreply.github.com>
2024-10-30 13:53:04 +0800

    alter record schema again

--- fhir_select.py
+++ fhir_select.py
@@ -24,21 +24,18 @@ async def process_bundle(bundle_path):
         extract_patient_data(bundle)
     )
 
-    patient_text = f"Patient {patient_data["name"][0]["given"][0]} \
-        {patient_data["name"][0]["family"]}\
-        gender {patient_data["gender"]}\
-        born {patient_data["birthDate"]}\
-        {patient_data["maritalStatus"]["text"]} " + (
-        f"deceased {patient_data.get("deceasedDateTime", "")}"
-        if "deceasedDateTime" in patient_data or "deceasedBoolean" in patient_data
-        else "alive"
+    patient_text = (
+        f"Patient {patient_data["name"][0]["given"][0]} {patient_data["name"][0]["family"]} gender {patient_data["gender"]} born {patient_data["birthDate"]} {patient_data["maritalStatus"]["text"]} "
+        + (
+            f"deceased {patient_data.get("deceasedDateTime", "")}"
+            if "deceasedDateTime" in patient_data or "deceasedBoolean" in patient_data
+            else "alive"
+        )
     )
-    observation_text = " ".join(
+    observation_text = " | ".join(
         [extract_observation_value(obs) for obs in observations]
     )
-    condition_texts = " ".join(
-        [cond.get("code", {}).get("text", "") for cond in conditions]
-    )
+    condition_texts = " ".join([extract_condition(cond) for cond in conditions])
     medication_texts = " ".join([extract_medication_value(med) for med in medications])
 
     allergy_texts = " ".join(
@@ -52,11 +49,11 @@ async def process_bundle(bundle_path):
         "allergies": [extract_allergy_information(allergy) for allergy in allergies],
     }
 
-    # procedure_texts = " ".join(
-    #    [extract_procedure_value(procedure) for procedure in procedures]
-    # )
+    procedure_texts = " ".join(
+        [extract_procedure_value(procedure) for procedure in procedures]
+    )
 
-    combined_texts = f"{patient_text}\nObservations: {observation_text}\nConditions: {condition_texts}\nMedications: {medication_texts}\nAllergies: {allergy_texts}"
+    combined_texts = f"{patient_text}\nObservations: {observation_text}\nConditions: {condition_texts}\nProcedures: {procedure_texts}\nMedications: {medication_texts}\nAllergies: {allergy_texts}"
 
     patient_object = {
         "id": patient_data["id"],
@@ -64,10 +61,15 @@ async def process_bundle(bundle_path):
         "metadata": metadata,
     }
 
-    async with aiofiles.open(
-        f"{FLAT_FILE_PATH}/{file_name}_processed.json", "w"
-    ) as out:
-        await out.write(json.dumps(patient_object, indent=4))
+    # write as json
+    # async with aiofiles.open(
+    #     f"{FLAT_FILE_PATH}/{file_name}_processed.json", "w"
+    # ) as out:
+    #     await out.write(json.dumps(patient_object, indent=4))
+
+    # write to text
+    async with aiofiles.open(f"{FLAT_FILE_PATH}/{file_name}_processed.txt", "w") as out:
+        await out.write(combined_texts)
 
 
 def extract_allergy_information(allergy):
@@ -78,55 +80,57 @@ def extract_medication_value(medication):
     medication_name = medication.get("medicationCodeableConcept", {}).get("text", "")
     status = medication.get("status", "")
 
-    return f"{medication_name} {status}"
+    return f"{status} {medication_name}"
+
+
+def extract_condition(entry):
+    name = entry["code"]["text"]
+    recorded_date = datetime.fromisoformat(entry["recordedDate"]).strftime("%Y/%m/%d")
+    return f"{name} recorded {recorded_date}"
 
 
 def extract_procedure_value(procedure):
     name = procedure["code"]["text"]
     status = procedure["status"]
-    period = ""
     if "performedPeriod" in procedure:
         start_date = datetime.fromisoformat(
             procedure["performedPeriod"]["start"]
-        ).strftime("%Y:%m:%d")
+        ).strftime("%Y/%m/%d")
         end_date = datetime.fromisoformat(procedure["performedPeriod"]["end"]).strftime(
-            "%Y:%m:%d"
+            "%Y/%m/%d"
         )
 
-        period = f"{start_date}-{end_date}"
-
-    return f"{name} {status} {period}"
-
-
-def extract_observation_value(observation):
-    """Extract the main value from an Observation based on available fields."""
-    if "valueQuantity" in observation:
-        # Extract numerical value with unit
-        quantity = observation["valueQuantity"]
-        text = observation["code"]["text"]
-        date_time = datetime.fromisoformat(observation["issued"]).strftime("%Y:%m:%d")
-
-        return (
-            f"{text} {quantity['value']} {quantity['unit']} {date_time}"
-            if "unit" in quantity
-            else f"{text} {quantity["value"]}"
-        )
-
-    elif "valueCodeableConcept" in observation:
-        # Extract coded value (e.g., LOINC code description)
-        return observation["valueCodeableConcept"]["text"]
-
-    elif "valueBoolean" in observation:
-        # Extract boolean value
-        return "True" if observation["valueBoolean"] else "False"
-
-    elif "valueString" in observation:
-        # Extract plain text value
-        return observation["valueString"]
-
+        if status == "completed":
+            return f" {status} {name} at {end_date}"
+        else:
+            return f"{status} {name} from {start_date}"
     else:
-        # If no recognizable value field is present
-        return "None"
+        return f"{status} {name}"
+
+
+def extract_observation_value(entry):
+    """Extract the main value from an Observation based on available fields."""
+    status = entry["status"]
+    category = entry["category"][0]["coding"][0]["display"]
+    code = entry["code"]["coding"][0]["display"]
+    effective_time = datetime.fromisoformat(entry["effectiveDateTime"]).strftime(
+        "%d/%m/%Y %H:%M:%S"
+    )
+    issued = datetime.fromisoformat(entry["issued"]).strftime("%d/%m/%Y %H:%M:%S")
+    if "valueQuantity" in entry:
+        value = format(
+            f"{entry["valueQuantity"]["value"]:.2f} {entry["valueQuantity"]["unit"]}"
+        )
+    elif "valueCodeableConcept" in entry:
+        # Extract coded value (e.g., LOINC code description)
+        value = entry["valueCodeableConcept"]["text"]
+    elif "valueBoolean" in entry:
+        value = "True" if entry["valueBoolean"] else "False"
+    elif "valueString" in entry:
+        value = entry["valueString"]
+    else:
+        value = "None"
+    return f"Category is {category}. Code is {code}. Effective on {effective_time}. Issued {issued}. Value is {value}."
 
 
 def extract_patient_data(fhir_data):
