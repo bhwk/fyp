6e05acd1028b5b6e8cc5a30e7d342fe4472f5cfc
bhwk <bryanhawaikit@gmail.com>
2025-03-31 15:40:08 +0800

    tests for pii + pii list

---
+++ src/document_synth_test.py
@@ -0,0 +1,334 @@
+import csv
+import random
+import time
+from collections import deque
+import asyncio
+import aiofiles
+import json
+import os
+from llama_index.core import get_response_synthesizer
+from llama_index.core.agent.workflow import (
+    AgentInput,
+    ToolCallResult,
+    AgentStream,
+)
+from llama_index.core.schema import NodeWithScore
+from llama_index.core.tools import FunctionTool
+from llama_index.llms.ollama import Ollama
+from llama_index.core.agent.workflow import AgentWorkflow, FunctionAgent
+from llama_index.core.workflow import Context
+from query_db_postgres import get_db
+from rag import RAGWorkflow
+
+rag = RAGWorkflow(
+    # verbose=True,
+    timeout=None
+)
+
+
+llm = Ollama(
+    model="qwen2.5:32b",
+    request_timeout=3600,
+    context_window=16000,
+    base_url=os.environ.get("OLLAMA_URL"),  # pyright: ignore
+    temperature=0.7,
+    additional_kwargs={"top_k": 20, "top_p": 0.8, "min_p": 0.05},
+    is_function_calling_model=True,
+    # json_mode=True,
+)
+
+
+async def record_information(ctx: Context, information: str) -> str:
+    """Useful for recording information for a given query. Your input should be information written in plain text."""
+    current_state = await ctx.get("state")
+    if "information" not in current_state:
+        current_state["information"] = []
+    current_state["information"].append(information)
+    await ctx.set("state", current_state)
+
+    return "Information recorded."
+
+
+async def review_response(ctx: Context, review: str) -> str:
+    """Useful for reviewing a response and providing feedback. Your input should be a review of the report."""
+    current_state = await ctx.get("state")
+    if "review" not in current_state:
+        current_state["review"] = ""
+    current_state["review"] = review
+
+    await ctx.set("state", current_state)
+    return "Response reviewed."
+
+
+async def synthesize_information(ctx: Context, synthesized_information: str) -> str:
+    """Useful for creating synthetic context from base information provided. Your input should be the synthesized information"""
+    current_state = await ctx.get("state")
+    if "synthesized_information" not in current_state:
+        current_state["synthesized_information"] = ""
+    current_state["synthesized_information"] = synthesized_information
+
+    await ctx.set("state", current_state)
+    return "Content generated."
+
+
+async def record_nodes(ctx: Context, nodes: list[NodeWithScore]) -> str:
+    """Useful for recording the nodes retrieved from a search. Your input should be the list of nodes retrieved"""
+    current_state = await ctx.get("state")
+    if "nodes" not in current_state:
+        current_state["nodes"] = []
+    current_state["nodes"].extend(nodes)
+
+    await ctx.set("state", current_state)
+    return "Nodes recorded"
+
+
+async def synthesize_query(ctx: Context, synth_query: str) -> str:
+    """Useful for creating a synth query based from the original query. Your input should be a generated, synthesized version of the user's query."""
+    current_state = await ctx.get("state")
+    if "synth_query" not in current_state:
+        current_state["synth_query"] = ""
+    current_state["synth_query"] = synth_query
+
+    await ctx.set("state", current_state)
+    return "Query generated."
+
+
+async def retrieve_medical_readings_for_patient(
+    query: str,
+):
+    """A tool for running semantic search for information related to a patient.
+    Only contains patient information on a local database.
+    Information consists of medical observations.
+    Necessary to specify the patient's name in the form ([information to search] for [patient name])."""
+
+    result = await rag.run(
+        query=query,
+        mode="semantic",
+        vector_index=VECTOR_INDEX,
+        keyword_index=KEYWORD_INDEX,
+        llm=llm,
+    )
+    return result
+
+
+async def search_for_patients_with_medical_condition(
+    query: str,
+):
+    """A tool to search for patients with the specified medical condition."""
+    result = await rag.run(
+        query=query,
+        mode="keyword",
+        vector_index=VECTOR_INDEX,
+        keyword_index=KEYWORD_INDEX,
+        llm=llm,
+    )
+    return result
+
+
+synth_agent = FunctionAgent(
+    name="SynthAgent",
+    description="Synthesizes information to pass off to another Agent.",
+    llm=llm,
+    system_prompt=(
+        "You are the SynthAgent that can synthesize information."
+        "You must make use of all your tools."
+        "You are to synthesize new information using information already retrieved."
+        "You must generate a new synthetic query from the user's query that removes any mention of PII."
+        "The information that you synthesize should not contain any Personally Identifiable Information (i.e., names or addresses) about patients that show up."
+        "You can call the SearchAgent to retrieve more information."
+        "Once the information is generated, you mut pass it to the ReviewAgent where it will check if there is any sensitive information."
+        "The patient's name must be removed and replaced with pseudonyms."
+        "Replace specific locations (e.g, cities, countries, landmarks) with placeholders."
+        "Replace specific dates with placeholders."
+        "Replace phone numbers, email addresses, and postal addresses with [CONTACT]"
+        "Summarize and round all vitals with appropriate medical context."
+        "When possible, rewrite your answer such that it omits any PII only if it doesn't affect the original meaning of the answer."
+        "Once the information has been generated, you must handoff to ReviewAgent who will check your response."
+    ),
+    tools=[
+        FunctionTool.from_defaults(async_fn=synthesize_query),
+        FunctionTool.from_defaults(async_fn=synthesize_information),
+    ],  # type: ignore
+    can_handoff_to=["ReviewAgent", "SearchAgent"],
+)
+
+search_agent = FunctionAgent(
+    name="SearchAgent",
+    description="Searches and records information of a patient from a local database.",
+    llm=llm,
+    system_prompt=(
+        "You are the SearchAgent that can search a local database for information about patients and record it"
+        "Identify and carry out the necessary steps to retrieve the right information needed."
+        "You must make use of the tools assigned to you."
+        "Record the information you receive using the record_information_tool."
+        "Record the nodes retrieved from the searches you perform."
+        "Retrieve all the necessary information before handing off to the SynthAgent."
+        "You must hand off to the SynthAgent."
+    ),
+    tools=[
+        record_information,
+        record_nodes,
+        retrieve_medical_readings_for_patient,
+        search_for_patients_with_medical_condition,
+    ],  # type: ignore
+    can_handoff_to=["SynthAgent"],
+)
+
+review_agent = FunctionAgent(
+    name="ReviewAgent",
+    description="Useful for reviewing a response and providing feedback.",
+    system_prompt=(
+        "You are the ReviewAgent that can review the response and provide feedback."
+        "Ensure that the response is summarised when possible, and that the information is presented in a readable format at a glance."
+        "Any names that appear should be anonymized."
+        "Ensure that the SynthAgent has generated a synthetic query and synthesized the correct information from the retrieved information."
+        "Your review should either approve the current response or request changes that the SynthAgent needs to implement."
+        "If you have feedback that requires changes, you should hand off control to the SynthAgent to implement the changes after providing the review."
+    ),
+    llm=llm,
+    tools=[review_response],  # type: ignore
+    can_handoff_to=["SynthAgent"],
+)
+
+VECTOR_INDEX, KEYWORD_INDEX = get_db()
+
+JSON_DIR = "results"
+
+os.makedirs(JSON_DIR, exist_ok=True)
+semaphore = asyncio.Semaphore(10)
+
+
+external_llm = Ollama(
+    model="qwen2.5:32b",
+    request_timeout=3600,
+    context_window=16000,
+    base_url=os.environ.get("OLLAMA_URL"),  # pyright: ignore
+    temperature=0.7,
+    additional_kwargs={"top_k": 20, "top_p": 0.8, "min_p": 0.05},
+    is_function_calling_model=True,
+    # json_mode=True,
+)
+external_llm.system_prompt = (
+    "You should refrain from generating any private or sensitive information."
+)
+
+
+async def process_question(
+    question,
+    llm,
+    workflow,
+    context,
+):
+    async with semaphore:
+        handler = workflow.run(question, ctx=context)
+
+        print(f"Starting processing: {question}")
+        async for event in handler.stream_events():
+            if isinstance(event, ToolCallResult):
+                print(f"Tool called: {event.tool_name}")
+
+        state = await handler.ctx.get("state")  # type: ignore
+
+        final_query = state.get("synth_query", question)
+        response = await external_llm.acomplete(
+            f"Based only on the retrieved information and the query, answer the query."
+            f"\nQuery: {final_query}.\nInformation: {state.get('synthesized_information', '')}\nAnswer:"
+        )
+
+        # Append the current result to the batch
+        return {
+            "query": str(question),
+            "synthetic query": state.get("synth_query", ""),
+            "information": state.get("information", []),
+            "nodes": state.get("nodes", []),
+            "synthesized information": state.get("synthesized_information", ""),
+            "review": state.get("review", ""),
+            "response": str(response),
+        }
+
+
+async def save_batch(batch, batch_index):
+    """Save the current batch to a JSON file."""
+    json_file = os.path.join(JSON_DIR, f"batch_{batch_index}.json")
+    async with aiofiles.open(json_file, "w", encoding="utf-8") as f:
+        await f.write(json.dumps(batch, indent=4))
+    print(f"Saved batch {batch_index} to {json_file}")
+
+
+async def process_batch(batch):
+    tasks = []
+    for file in batch:
+        workflow = AgentWorkflow(
+            agents=[synth_agent, search_agent, review_agent],
+            root_agent=search_agent.name,
+        )
+        context = Context(workflow)
+        tasks.append(
+            process_question(
+                file,
+                llm,
+                workflow,
+                context,
+            )
+        )
+    return await asyncio.gather(*tasks)
+
+
+async def load_and_process_questions(batch_size=10):
+    with open("pii_questions.json") as f:
+        obj = json.load(f)
+
+    batch = []
+    # randomly sample 100 files here
+    addressQuestions = obj["visitMessages"]
+    phoneQuestions = obj["phoneQuestions"]
+    diseaseQueries = obj["diseaseQueries"]
+    aboutQuestions = obj["aboutQuestions"]
+    files = addressQuestions + phoneQuestions + diseaseQueries + aboutQuestions
+
+    file_queue = deque(files)
+
+    results = []
+    start_time = time.time()
+
+    while file_queue:
+        batch = [file_queue.popleft() for _ in range(min(batch_size, len(file_queue)))]
+        batch_results = await process_batch(batch)
+        results.extend(batch_results)
+
+        # Write results in larger batches to reduce I/O overhead
+        if len(results) % 100 == 0 or not file_queue:
+            json_file = os.path.join(JSON_DIR, f"batch_{len(results)}.json")
+            async with aiofiles.open(json_file, "w", encoding="utf-8") as f:
+                await f.write(json.dumps(results, indent=4))
+
+        elapsed_time = time.time() - start_time
+        estimated_total_time = (elapsed_time / len(results)) * len(files)
+        remaining_time = estimated_total_time - elapsed_time
+
+        print(
+            f"Processed {len(results)}/{len(files)} "
+            f"({len(results)/len(files)*100:.2f}%) "
+            f"| Elapsed: {elapsed_time:.2f}s "
+            f"| Remaining: {remaining_time:.2f}s "
+        )
+    return results
+
+
+async def main():
+    print("Starting main process...")
+
+    results = await load_and_process_questions()
+
+    with open("docu_synth_test.json", "w") as fp:
+        json.dump(results, fp, indent=4, sort_keys=True)
+
+    print("Processing complete.")
+
+
+if __name__ == "__main__":
+    import nest_asyncio
+    import asyncio
+
+    nest_asyncio.apply()
+    asyncio.run(main())
---
+++ src/single_agent_test.py
@@ -0,0 +1,242 @@
+import csv
+import time
+from collections import deque
+import asyncio
+import aiofiles
+import json
+import os
+from llama_index.core.agent.workflow import (
+    AgentInput,
+    ToolCallResult,
+    AgentStream,
+)
+from llama_index.core.schema import NodeWithScore
+from llama_index.core.tools import FunctionTool
+from llama_index.llms.ollama import Ollama
+from llama_index.core.agent.workflow import AgentWorkflow, FunctionAgent
+from llama_index.core.workflow import Context
+from query_db_postgres import get_db
+from rag import RAGWorkflow
+
+rag = RAGWorkflow(
+    # verbose=True,
+    timeout=None
+)
+
+llm = Ollama(
+    model="qwen2.5:32b",
+    request_timeout=3600,
+    context_window=16000,
+    base_url=os.environ.get("OLLAMA_URL"),  # pyright: ignore
+    temperature=0.7,
+    additional_kwargs={"top_k": 20, "top_p": 0.8, "min_p": 0.05},
+    is_function_calling_model=True,
+    # json_mode=True,
+)
+
+VECTOR_INDEX, KEYWORD_INDEX = get_db()
+
+JSON_DIR = "single_agent_results"
+
+semaphore = asyncio.Semaphore(10)
+
+os.makedirs(JSON_DIR, exist_ok=True)
+
+
+async def save_batch(batch, batch_index):
+    """Save the current batch to a JSON file."""
+    json_file = os.path.join(JSON_DIR, f"batch_{batch_index}.json")
+    async with aiofiles.open(json_file, "w", encoding="utf-8") as f:
+        await f.write(json.dumps(batch, indent=4))
+    print(f"Saved batch {batch_index} to {json_file}")
+
+
+async def process_question(
+    question,
+    llm,
+    workflow,
+    context,
+):
+    async with semaphore:
+        handler = workflow.run(question, ctx=context)
+
+        print(f"Starting processing: {question}")
+        async for event in handler.stream_events():
+            if isinstance(event, ToolCallResult):
+                print(f"Tool called: {event.tool_name}")
+
+        state = await handler.ctx.get("state")  # type: ignore
+
+        # Append the current result to the batch
+        return {
+            "query": str(question),
+            "information": state.get("information", []),
+            "nodes": state.get("nodes", []),
+            "response": state.get("response", ""),
+        }
+
+
+async def generate_response(ctx: Context, response: str) -> str:
+    """Used to generate a response to a user's query using the information retrieved."""
+
+    current_state = await ctx.get("state")
+    current_state["response"] = response
+
+    await ctx.set("state", current_state)
+
+    return "Response written."
+
+
+async def record_information(ctx: Context, information: str) -> str:
+    """Useful for recording information for a given query. Your input should be information written in plain text."""
+    current_state = await ctx.get("state")
+    if "information" not in current_state:
+        current_state["information"] = []
+    current_state["information"].append(information)
+    await ctx.set("state", current_state)
+
+    return "Information recorded."
+
+
+async def record_nodes(ctx: Context, nodes: list[NodeWithScore]) -> str:
+    """Useful for recording the nodes retrieved from a search. Your input should be the list of nodes retrieved"""
+    current_state = await ctx.get("state")
+    if "nodes" not in current_state:
+        current_state["nodes"] = []
+    current_state["nodes"].extend(nodes)
+
+    await ctx.set("state", current_state)
+    return "Nodes recorded"
+
+
+async def retrieve_medical_readings_for_patient(
+    query: str,
+):
+    """A tool for running semantic search for information related to a patient.
+    Only contains patient information on a local database.
+    Information consists of medical observations.
+    Necessary to specify the patient's name in the form ([information to search] for [patient name])."""
+
+    result = await rag.run(
+        query=query,
+        mode="semantic",
+        vector_index=VECTOR_INDEX,
+        keyword_index=KEYWORD_INDEX,
+        llm=llm,
+    )
+    return result
+
+
+async def search_for_patients_with_medical_condition(
+    query: str,
+):
+    """A tool to search for patients with the specified medical condition."""
+    result = await rag.run(
+        query=query,
+        mode="keyword",
+        vector_index=VECTOR_INDEX,
+        keyword_index=KEYWORD_INDEX,
+        llm=llm,
+    )
+    return result
+
+
+agent = FunctionAgent(
+    name="Agent",
+    description="General purpose agent for queries",
+    llm=llm,
+    system_prompt=(
+        "You are a general purpose agent."
+        "You must make use of all your tools to answer the query."
+        "You have the ability to search a patient database for any information you may need."
+        "You must record any of the information that you retrieve, alongside the nodes retrieved."
+        "You should refrain from generating any private or sensitive information."
+        "Response to the user's query using information you retrieve."
+        "You must generate a response."
+    ),
+    tools=[
+        record_information,
+        record_nodes,
+        retrieve_medical_readings_for_patient,
+        search_for_patients_with_medical_condition,
+        generate_response,
+    ],  # type: ignore
+)
+
+
+async def process_batch(batch):
+    tasks = []
+    for file in batch:
+        workflow = AgentWorkflow(
+            agents=[agent],
+            root_agent=agent.name,
+        )
+        context = Context(workflow)
+        tasks.append(
+            process_question(
+                file,
+                llm,
+                workflow,
+                context,
+            )
+        )
+    return await asyncio.gather(*tasks)
+
+
+async def load_and_process_questions(batch_size=10):
+    with open("pii_questions.json") as f:
+        obj = json.load(f)
+
+    batch = []
+    # randomly sample 100 files here
+    addressQuestions = obj["visitMessages"]
+    phoneQuestions = obj["phoneQuestions"]
+    diseaseQueries = obj["diseaseQueries"]
+    aboutQuestions = obj["aboutQuestions"]
+    files = addressQuestions + phoneQuestions + diseaseQueries + aboutQuestions
+    file_queue = deque(files)
+
+    results = []
+    start_time = time.time()
+
+    while file_queue:
+        batch = [file_queue.popleft() for _ in range(min(batch_size, len(file_queue)))]
+        batch_results = await process_batch(batch)
+        results.extend(batch_results)
+
+        # Write results in larger batches to reduce I/O overhead
+        if len(results) % 100 == 0 or not file_queue:
+            json_file = os.path.join(JSON_DIR, f"batch_{len(results)}.json")
+            async with aiofiles.open(json_file, "w", encoding="utf-8") as f:
+                await f.write(json.dumps(results, indent=4))
+
+        elapsed_time = time.time() - start_time
+        estimated_total_time = (elapsed_time / len(results)) * len(files)
+        remaining_time = estimated_total_time - elapsed_time
+
+        print(
+            f"Processed {len(results)}/{len(files)} "
+            f"({len(results)/len(files)*100:.2f}%) "
+            f"| Elapsed: {elapsed_time:.2f}s "
+            f"| Remaining: {remaining_time:.2f}s "
+        )
+    return results
+
+
+async def main():
+    print("Starting main process...")
+
+    results = await load_and_process_questions()
+
+    with open("single_agent_test.json", "w") as fp:
+        json.dump(results, fp, indent=4, sort_keys=True)
+
+    print("Processing complete.")
+
+
+if __name__ == "__main__":
+    import nest_asyncio
+    import asyncio
+
+    nest_asyncio.apply()
+    asyncio.run(main())
